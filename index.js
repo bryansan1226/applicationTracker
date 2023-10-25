6fe71386a50f67bd75ac0a90940165eb4a87abea
bryansan1226 <70733299+bryansan1226@users.noreply.github.com>
2023-10-25 19:12:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get("/applications", (req, res) => {
 });
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 const PORT = process.env.PORT || 3001;
